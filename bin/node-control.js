8e0c9a4c5a03f5c46c172476f99a6b7a0ea701c6
Nikita Rukavkov <nrukavkov@appulate.com>
2021-03-04 12:54:21 +0300

    Added base nginx installer

--- bin/node-control.js
+++ bin/node-control.js
@@ -4,12 +4,15 @@ var args = process.argv.splice(process.execArgv.length + 2);
 
 command = args[0];
 
+const path = require('path');
+global.appRoot = path.join(path.resolve(__dirname),"..");
+
 switch (command) {
     case 'start':
-        require("../lib/start.js").exec();
+        require("../lib/nginx/start.js").exec();
         break;
     case 'stop':
-        require("../lib/stop.js").exec();
+        require("../lib/nginx/stop.js").exec();
         break;
     default:
         break;
