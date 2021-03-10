7d3668ec331e174512956e75e128c0945f385969
Nikita Rukavkov <nrukavkov@appulate.com>
2021-03-10 15:53:45 +0300

    Revert matrix.os in arango job

--- config.arangodb.js
+++ config.arangodb.js
@@ -1,15 +1,15 @@
 module.exports = (version) => {
     const basePath = "https://download.arangodb.com/arangodb37/Community";
     return {
-        "ubuntu" : { 
+        "Linux" : { 
             "url": `${basePath}/Linux/arangodb3-linux-${version}.tar.gz`, 
             "pathInArchive": `arangodb3-linux-${version}`,
         },
-        "windows" : {
+        "Windows" : {
             "url": `${basePath}/Windows/ArangoDB3-${version}_win64.zip`, 
             "pathInArchive": `ArangoDB3-${version}_win64`
         },
-        "macos" : {
+        "macOS" : {
             "url": `${basePath}/MacOSX/arangodb3-macos-${version}.tar.gz`, 
             "pathInArchive": `arangodb3-macos-${version}`
         }
