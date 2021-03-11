c4e311e355a4ab45b7b24419262351c1829a331a
Nikita Rukavkov <nrukavkov@appulate.com>
2021-03-11 21:05:08 +0300

    nginx implementation

--- bin/node-control.js
+++ bin/node-control.js
@@ -1,18 +1,24 @@
 #!/usr/bin/env node
 
+require('dotenv').config()
 var args = process.argv.splice(process.execArgv.length + 2);
 
 command = args[0];
 
 const path = require('path');
 global.appRoot = path.join(path.resolve(__dirname),"..");
+global.serverPath = path.join(appRoot,".server");
+global.appsPath = path.join(serverPath,"apps");
+global.cachePath = path.join(serverPath,"cache");
+global.dataPath = path.join(serverPath,"data");
+global.logsPath = path.join(serverPath,"logs");
 
 switch (command) {
     case 'start':
-        require("../lib/nginx/serviceControl.js").start();
+        require("../lib/serverManagement/management").start();
         break;
     case 'stop':
-        require("../lib/nginx/serviceControl.js").stop();
+        require("../lib/serverManagement/management").stop();
         break;
     default:
         break;
