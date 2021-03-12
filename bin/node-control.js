439088f0613c7f35f1cea31ffda1a3594647766d
Alexander Salamatov <ch1seL@ya.ru>
2021-03-12 13:03:50 +0300

    fix global and remove arango with error

--- bin/node-control.js
+++ bin/node-control.js
@@ -1,18 +1,12 @@
 #!/usr/bin/env node
 
 require('dotenv').config();
-const path = require('path');
+require('../lib/global.js');
+
 const managment = require('../lib/serverManagement/management');
 
 const args = process.argv.splice(process.execArgv.length + 2);
 const command = args[0];
-
-global.appRoot = path.join(path.resolve(__dirname), '..');
-global.serverPath = path.join(global.appRoot, '.server');
-global.appsPath = path.join(global.serverPath, 'apps');
-global.cachePath = path.join(global.serverPath, 'cache');
-global.dataPath = path.join(global.serverPath, 'data');
-global.logsPath = path.join(global.serverPath, 'logs');
 
 switch (command) {
   case 'start':
