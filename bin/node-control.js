5c4e72f4b254091f42f42b7e0ae1bf9ddfcc6379
Alexander Salamatov <ch1seL@ya.ru>
2021-03-12 17:02:11 +0300

    fix type

--- bin/node-control.js
+++ bin/node-control.js
@@ -6,17 +6,17 @@ require('../lib/global.js');
 require('dotenv').config();
 require('dotenv').config({ path: path.join(global.appRoot, '.env.settings') });
 
-const managment = require('../lib/serverManagement/management');
+const management = require('../lib/serverManagement/management');
 
 const args = process.argv.splice(process.execArgv.length + 2);
 const command = args[0];
 
 switch (command) {
   case 'start':
-    managment.start();
+    management.start();
     break;
   case 'stop':
-    managment.stop();
+    management.stop();
     break;
   default:
     break;
