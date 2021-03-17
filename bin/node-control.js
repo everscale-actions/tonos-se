6acb67c8e98f87ddd31b9a68963d075fe5978213
Nikita Rukavkov <nrukavkov@appulate.com>
2021-03-17 19:07:46 +0300

    base config logic

--- bin/node-control.js
+++ bin/node-control.js
@@ -6,24 +6,43 @@ require('../lib/global.js');
 require('dotenv').config();
 require('dotenv').config({ path: path.join(global.appRoot, '.env.settings') });
 
-const management = require('../lib/control');
-
-const args = process.argv.splice(process.execArgv.length + 2);
-const command = args[0];
+const commandLineArgs = require('command-line-args');
+const control = require('../lib/control');
+/* first - parse the main command */
+const mainDefinitions = [
+  { name: 'command', defaultOption: true },
+];
+const mainOptions = commandLineArgs(mainDefinitions, { stopAtFirstUnknown: true });
+// eslint-disable-next-line no-underscore-dangle
+const argv = mainOptions._unknown || [];
 
 async function main() {
-  switch (command) {
+  switch (mainOptions.command) {
     case 'start':
-      await management.start();
+      await control.start();
       break;
     case 'stop':
-      await management.stop();
+      await control.stop();
       break;
     case 'reset':
-      await management.reset();
+      await control.reset();
       break;
     case 'status':
-      await management.status();
+      await control.status();
+      break;
+    case 'config':
+      // eslint-disable-next-line no-case-declarations
+      const mergeDefinitions = [
+        { name: 'nginx-port', type: Number },
+        { name: 'arango-port', type: Number },
+        { name: 'node-port', type: Number },
+        { name: 'qserver-port', type: Number },
+      ];
+
+      // eslint-disable-next-line no-case-declarations
+      const newSettings = commandLineArgs(mergeDefinitions, { argv });
+
+      control.config(newSettings);
       break;
     default:
       break;
