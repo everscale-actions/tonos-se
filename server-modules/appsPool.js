046c8aad883377a12edc69987f042400ea11a18e
Alexander Salamatov <ch1seL@ya.ru>
2021-03-18 23:31:13 +0300

    transform before starting

--- server-modules/appsPool.js
+++
@@ -1,6 +0,0 @@
-const nginx = require('./nginx/module');
-const arango = require('./arango/module');
-const qServer = require('./q-server/module');
-const tonNode = require('./ton-node/module');
-
-module.exports.appsPool = [arango, tonNode, qServer, nginx];
